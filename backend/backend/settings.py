78734a347d692252b57b2f1b61e25f730ece0e2f
k0shka <jopsnowball2017@gmail.com>
2024-08-03 16:13:21 +0800

    Add: GraphQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'ckeditor',
+    'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -147,3 +150,12 @@ AUTH_USER_MODEL = 'blog.User'
 
 # CKEditor configs
 CKEDITOR_BASEPATH = "static/ckeditor/ckeditor/"
+
+# GraphQL Configuration
+GRAPHENE = {
+    "SCHEMA": "blog.schema.schema",
+}
+
+# Cors 
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = ("http://localhost:8080",)
